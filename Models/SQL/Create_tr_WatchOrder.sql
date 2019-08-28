070203d7304b6f054c3a9f769a39c8ae0caa3d32
Philip Miller <phil.miller84@gmail.com>
2019-08-28 16:15:33 -0700

    Update Strategy status on order completion
    Update Strategy status on order completion

--- Models/SQL/Create_tr_WatchOrder.sql
+++ Models/SQL/Create_tr_WatchOrder.sql
@@ -1,4 +1,4 @@
-CREATE TRIGGER [dbo].[tr_WatchOrder] ON dbo.Orders AFTER INSERT
+CREATE TRIGGER [dbo].[tr_WatchOrder] ON dbo.Orders AFTER INSERT, UPDATE
 AS  
 DECLARE @OrderID AS int 
 DECLARE @Type 		AS int
@@ -8,6 +8,25 @@ SELECT @OrderID 	= OrderId,
        @Type 		= Type,
        @Status 		= Status 	
 FROM INSERTED	   
+	
+IF @Status = 2
+BEGIN
+	;
+	WITH StrategyStatus(OrderId, OrderStatus, StrategyId) AS
+	(
+		SELECT o2.OrderId, o2.Status, s.StrategyId
+		FROM dbo.StrategyOrderJoins so
+		JOIN dbo.StrategyOrderJoins so2 ON so.StrategyId = so2.StrategyId
+		JOIN dbo.Orders o2 ON so2.OrderId = o2.OrderId
+		JOIN dbo.Strategies  s ON so.StrategyId = s.StrategyId
+		WHERE so.OrderId = @OrderID
+	)
+	UPDATE dbo.Strategies  
+	SET Status = 2
+	FROM dbo.Strategies  s
+	JOIN StrategyStatus ON s.StrategyId = StrategyStatus.StrategyId
+	WHERE NOT EXISTS (SELECT 1 FROM StrategyStatus WHERE OrderStatus <> 2)
+END
 
 
 
